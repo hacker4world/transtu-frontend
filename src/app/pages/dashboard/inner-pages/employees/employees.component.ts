ee35979cd00661700894318cb0e36ba1199bac78
hacker4world <zonegaming514@gmail.com>
2025-03-13 03:39:06 +0100

    .....

--- src/app/pages/dashboard/inner-pages/employees/employees.component.ts
+++ src/app/pages/dashboard/inner-pages/employees/employees.component.ts
@@ -2,20 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EmployeesService } from '../../../services/employees.service';
 import { CommonModule } from '@angular/common';
-import { CreationModalComponent } from '../../components/creation-modal/creation-modal.component';
 import { FormsModule } from '@angular/forms';
 import { AgentService } from '../../../services/agent.service';
 
 @Component({
   selector: 'app-employees',
   standalone: true,
-  imports: [CommonModule, CreationModalComponent, FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.css',
 })
 export class EmployeesComponent implements OnInit {
-  agents: any[] = [];  
-  searchQuery: string = ''; 
+  agents: any[] = [];
+  searchQuery: string = '';
+  showAddModal = false;
+
+  showUpdateModal = false;
+
+  newAgent: any = {
+    nom: '',
+    prenom: '',
+    role: '',
+    departement: '',
+  };
+
+  roles = ['Chauffeur', 'Receveur'];
 
   constructor(private agentService: AgentService) {}
 
@@ -34,22 +45,23 @@ export class EmployeesComponent implements OnInit {
     );
   }
 
-
   filteredAgents() {
-    return this.agents.filter(agent => 
-      agent.nom.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-      agent.prenom.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-      agent.role.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-      agent.departement.toLowerCase().includes(this.searchQuery.toLowerCase())
+    return this.agents.filter(
+      (agent) =>
+        agent.nom.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        agent.prenom.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        agent.role.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        agent.departement.toLowerCase().includes(this.searchQuery.toLowerCase())
     );
   }
 
- 
   removeAgent(matricule: number): void {
     if (confirm('Voulez-vous vraiment supprimer cet agent ?')) {
       this.agentService.removeAgent(matricule).subscribe(
         () => {
-          this.agents = this.agents.filter(agent => agent.matricule !== matricule);
+          this.agents = this.agents.filter(
+            (agent) => agent.matricule !== matricule
+          );
           console.log('Agent supprimé avec succès.');
         },
         (error) => {
@@ -59,13 +71,11 @@ export class EmployeesComponent implements OnInit {
     }
   }
 
-  editAgent(agent: any): void {
-    console.log('Modification de l’agent:', agent);
-  
+  setShowModifyModal() {
+    this.showUpdateModal = true;
   }
 
   addAgent(): void {
-    console.log('Création d’un nouvel agent');
-  
+    this.showAddModal = true;
   }
 }
